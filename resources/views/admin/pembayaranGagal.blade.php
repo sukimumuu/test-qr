82660ccd7cbec531fe3a2fe95e4ef2e3099aef49
vindrayulian <vindrayulian@gmail.com>
2024-07-02 13:03:16 +0700

    style : change registration success and failed

--- resources/views/admin/pembayaranGagal.blade.php
+++ resources/views/admin/pembayaranGagal.blade.php
@@ -5,42 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Form FunRun</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link
-    href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css"
-    rel="stylesheet"
-/>
+    <link rel="stylesheet" href="{{ asset('asset/css/before-form/main.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet"/>
 </head>
   <body>
-
-    <section>
+    <div class="container-fluid funrun-registration">
         <div class="container">
             <div class="row">
-                <div class="card gap-4">
-                    <div class="p-3">
-                        <div class="row col-12 col-md-12 justify-content-between mx-auto">
-                            <div class="col-4 col-md-4">
-                                <img src="{{ asset('asset/img/rotary wheel.webp') }}" width="50px" alt="">
-                            </div>
-                            {{-- <div class="col-4 col-md-4">
-                                <span>Bank Indonesia</span>
-                            </div> --}}
-                            <div class="col-4 col-md-4 d-flex justify-content-end">
-                                <img src="{{ asset('asset/img/Logo Nemolab.png') }}" width="50px" alt="">
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-12 col-md-12">
 
-                    <div class="card-content">
-                        <div class="row">
-                            <i class="ri-close-circle-fill d-flex justify-content-center text-danger" style="font-size: 10rem"></i>
-                            <h3 class="fw-bold d-flex justify-content-center mb-3">Hii Sobat Lari pembayaran anda belum berhasil, Coba ulangi atau hubungi panitia</h3>
+                    <h3 class="text-center fw-bold mt-5 mb-3">Pendaftaran Event FunRun Rotary <br> Purwokerto 2024</h3>
+
+                    <div class="row col-12 col-md-12 mx-auto">
+                        <div class="card p-3 mb-3 gradient-background kaca">
+                            <div class="card-header" style="background: transparent; border: none; z-index: 2">
+                                <h3 class="text-dark">Registration</h3>
+                                <span class="border-bottom border-dark border-3 rounded-2" style="width: 120px"></span>
+                            </div>
+                            <div class="card-body" style="z-index: 2">
+                                <div class="row col-12 col-md-12">
+                                    <img src="{{ asset('asset/img/register-failed.png') }}" class="mx-auto" style="width: 400px" alt="">
+                                    <h3 class="fw-bold text-center">Yah Pembayaran Kamu Gagal</h3>
+                                    <div class="d-flex justify-content-center align-items-center gap-2">
+                                        <span>Silakan coba lagi pembayaran untuk melakukan pendaftaran</span>
+                                        <a href="" class="btn btn-submit">Kembali ke halaman pendaftaran</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
