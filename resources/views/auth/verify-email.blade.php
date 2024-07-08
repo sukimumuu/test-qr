0dab60044ea62b24660d8105df88aff20ac8f124
vindrayulian <vindrayulian@gmail.com>
2024-07-08 12:55:46 +0700

    style : add page email verifikations

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -3,18 +3,44 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Verifikasi Email</title>
+    <title>Form FunRun</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
+    <link rel="stylesheet" href="{{ asset('asset/css/before-form/main.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet"/>
+</head>
   <body>
-    <div class="position-absolute top-50 start-50 translate-middle">
-        <div class="card">
-        <div class="card-header">
-            Silahkan Cek Email Anda Untuk Memverifikasi Email
-        </div>
-        <a class="text-center m-3" href="{{ route('verification.send') }}"><button class="btn btn-secondary col-lg-6">Kirim Ulang Link</button></a>
+    <div class="container-fluid funrun-registration">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-12">
+
+                    <h3 class="text-center fw-bold mt-5 mb-3">Pendaftaran Event FunRun Rotary <br> Purwokerto 2024</h3>
+
+                    <div class="row col-12 col-md-12 mx-auto">
+                        <div class="card p-3 mb-3 gradient-background kaca">
+                            <div class="card-header" style="background: transparent; border: none; z-index: 2">
+                                <h3 class="text-dark">Registration</h3>
+                                <span class="border-bottom border-dark border-3 rounded-2" style="width: 120px"></span>
+                            </div>
+                            <div class="card-body" style="z-index: 2">
+                                <div class="row col-12 col-md-12">
+                                    <img src="{{ asset('asset/img/success.png') }}" class="mx-auto" style="width: 200px" alt="">
+                                    <h3 class="fw-bold text-center">Email Berhasil Dikirim!!</h3>
+                                    <div class="d-flex justify-content-center align-items-center">
+                                        <span>Silahkan cek Email anda untuk memverifikasi Email</span>
+                                    </div>
+                                    <div class="d-flex justify-content-center align-items-center mt-2">
+                                        <a href="" class="btn btn-submit">Kirim Ulang Link</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,18 @@ Route::get('/scan', function () {
     return view('admin.scan');
 });
 Route::get('/alpha-registration', function(){
-    return view('welcome');
+    return view('auth.regis');
 });
 Route::post('/storing-user', [RegistrationController::class, 'alphaReg'])->name('storing-users');
 Route::get('/email/verify', function () {
     return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
+})->name('verification.notice');
 
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
     return redirect('/');
 })->middleware(['auth', 'signed'])->name('verification.verify');
- 
+
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
     return back()->with('message', 'Verification link sent!');
