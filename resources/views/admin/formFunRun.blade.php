d42f285fe37bdf29a80416d19a0f1902fb68f6f8
vindrayulian <vindrayulian@gmail.com>
2024-07-08 11:59:42 +0700

    feat : config

--- resources/views/admin/formFunRun.blade.php
+++ resources/views/admin/formFunRun.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Form FunRun</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ secure_asset('asset/css/form/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('asset/css/form/main.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet"/>
     <link href="https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css" rel="stylesheet">
 </head>
